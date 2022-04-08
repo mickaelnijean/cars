2ab5951431870136d0fa35e04a3f2c974b596550
unknown <mickael.nijean@gmail.com>
2022-04-08 11:46:58 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CarRepositoryService } from './shared/services/database/car-repository.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'entretien';
+  cars = [];
+  constructor(public carRepositoryService: CarRepositoryService) {
+  }
+
+  ngOnInit(): void {
+    this.carRepositoryService.getAllCars()
+    .subscribe(cars => this.cars = cars);
+  }
 }
